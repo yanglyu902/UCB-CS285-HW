2cd87b216a782fe0fe59ecfe6378ef091df8b4a6
Leotrinos <yanglyu2@berkeley.edu>
2022-09-02 02:29:32 -0700

    implemented mlp

--- hw1/cs285/infrastructure/pytorch_util.py
+++ hw1/cs285/infrastructure/pytorch_util.py
@@ -47,7 +47,19 @@ def build_mlp(
 
     # TODO: return a MLP. This should be an instance of nn.Module
     # Note: nn.Sequential is an instance of nn.Module.
-    raise NotImplementedError
+
+    layers = []
+    layers.append(nn.Linear(input_size, size))
+    layers.append(activation)
+    for i in range(size):
+        layers.append(nn.Linear(size, size))
+        layers.append(activation)
+    layers.append(nn.Linear(size, output_size))
+    layers.append(output_activation)
+
+    mlp = nn.Sequential(*layers)
+
+    return mlp
 
 
 device = None
